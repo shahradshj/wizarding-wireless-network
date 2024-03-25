9ab51ef4ea6db80052d873daa5b896d12e8fdb40
Shahrad Laptop <shahrad.shj@gmail.com>
2024-03-25 13:20:26 -0700

    formatted main with cargo fmt

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,12 +1,12 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 extern crate rusqlite;
 
-use rocket::serde::Serialize;
 use rocket::serde::json::Json;
+use rocket::serde::Serialize;
 use rusqlite::{params, Connection};
 
 const DATABASE_PATH: &str = "../movie-database/database.db";
-
 
 #[derive(Serialize)]
 struct Movie {
@@ -41,13 +41,15 @@ struct Episode {
 fn get_movies() -> Json<Vec<Movie>> {
     let conn = Connection::open(DATABASE_PATH).unwrap();
     let mut stmt = conn.prepare("SELECT id, title, year FROM movies").unwrap();
-    let movies_iter = stmt.query_map(params![], |row| {
-        Ok(Movie {
-            id: row.get(0)?,
-            title: row.get(1)?,
-            year: row.get(2)?,
+    let movies_iter = stmt
+        .query_map(params![], |row| {
+            Ok(Movie {
+                id: row.get(0)?,
+                title: row.get(1)?,
+                year: row.get(2)?,
+            })
         })
-    }).unwrap();
+        .unwrap();
 
     let mut movies = Vec::new();
     for movie in movies_iter {
@@ -60,16 +62,20 @@ fn get_movies() -> Json<Vec<Movie>> {
 #[get("/series")]
 fn get_series() -> Json<Vec<Series>> {
     let conn = Connection::open(DATABASE_PATH).unwrap();
-    let mut stmt = conn.prepare("SELECT id, title, start_year, end_year FROM series").unwrap();
-    let series_iter = stmt.query_map(params![], |row| {
-        Ok(Series {
-            id: row.get(0)?,
-            title: row.get(1)?,
-            start_year: row.get(2)?,
-            end_year: row.get(3)?,
-            seasons: Vec::new(),
+    let mut stmt = conn
+        .prepare("SELECT id, title, start_year, end_year FROM series")
+        .unwrap();
+    let series_iter = stmt
+        .query_map(params![], |row| {
+            Ok(Series {
+                id: row.get(0)?,
+                title: row.get(1)?,
+                start_year: row.get(2)?,
+                end_year: row.get(3)?,
+                seasons: Vec::new(),
+            })
         })
-    }).unwrap();
+        .unwrap();
 
     let mut series = Vec::new();
     for serie in series_iter {
@@ -84,22 +90,29 @@ fn get_series_by_id(id: String) -> Json<Series> {
     let conn = Connection::open(DATABASE_PATH).unwrap();
     print!("Fetching series with id: {}\n", id);
     // Fetch episodes
-    let mut stmt = conn.prepare("SELECT id, season, episode FROM episodes WHERE series_id = ?1").unwrap();
+    let mut stmt = conn
+        .prepare("SELECT id, season, episode FROM episodes WHERE series_id = ?1")
+        .unwrap();
     print!("Fetching episodes\n");
-    let episodes_iter = stmt.query_map(params![id], |row| {
-        Ok(Episode {
-            id: row.get(0)?,
-            season: row.get(1)?,
-            episode: row.get(2)?,
+    let episodes_iter = stmt
+        .query_map(params![id], |row| {
+            Ok(Episode {
+                id: row.get(0)?,
+                season: row.get(1)?,
+                episode: row.get(2)?,
+            })
         })
-    }).unwrap();
+        .unwrap();
     print!("Episodes fetched\n");
 
     // Create seasons and attach episodes
     let mut seasons: Vec<Season> = Vec::new();
     for episode in episodes_iter {
         let episode = episode.unwrap();
-        print!("season id {:?} episode number {:?} id {:?}\n", episode.season, episode.episode, episode.id);
+        print!(
+            "season id {:?} episode number {:?} id {:?}\n",
+            episode.season, episode.episode, episode.id
+        );
         let season = seasons.iter_mut().find(|s| s.season == episode.season);
         match season {
             Some(season) => season.episodes.push(episode),
@@ -113,24 +126,29 @@ fn get_series_by_id(id: String) -> Json<Series> {
     }
     print!("number of seasons {:?}\n", seasons.len());
 
-    let mut stmt = conn.prepare("SELECT id, title, start_year, end_year FROM series WHERE id = ?1").unwrap();
-    let series = stmt.query_row(params![id], |row| {
-        Ok(Series {
-            id: row.get(0)?,
-            title: row.get(1)?,
-            start_year: row.get(2)?,
-            end_year: row.get(3)?,
-            seasons: seasons,
+    let mut stmt = conn
+        .prepare("SELECT id, title, start_year, end_year FROM series WHERE id = ?1")
+        .unwrap();
+    let series = stmt
+        .query_row(params![id], |row| {
+            Ok(Series {
+                id: row.get(0)?,
+                title: row.get(1)?,
+                start_year: row.get(2)?,
+                end_year: row.get(3)?,
+                seasons: seasons,
+            })
         })
-    }).unwrap();
-    print!("series id {:?} title {:?} start year {:?} end year {:?}\n", series.id, series.title, series.start_year, series.end_year);
+        .unwrap();
+    print!(
+        "series id {:?} title {:?} start year {:?} end year {:?}\n",
+        series.id, series.title, series.start_year, series.end_year
+    );
 
     Json(series)
 }
 
-
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .mount("/", routes![get_movies, get_series, get_series_by_id])
+    rocket::build().mount("/", routes![get_movies, get_series, get_series_by_id])
 }
