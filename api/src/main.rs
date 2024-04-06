9f640de855e0f8ea8930c75fcc5373d5c21adb08
Shahrad Windows-PC <shahrad.shj@gmail.com>
2024-04-06 13:07:52 -0700

    made endpoints async

--- api/src/main.rs
+++ api/src/main.rs
@@ -8,6 +8,7 @@ use rusqlite::{params, Connection};
 
 const DATABASE_PATH: &str = "../movie-database/database.db";
 
+#[derive(Debug)]
 #[derive(Serialize)]
 struct Movie {
     id: String,
@@ -15,6 +16,7 @@ struct Movie {
     year: i32,
 }
 
+#[derive(Debug)]
 #[derive(Serialize)]
 struct Series {
     id: String,
@@ -24,12 +26,14 @@ struct Series {
     seasons: Vec<Season>,
 }
 
+#[derive(Debug)]
 #[derive(Serialize)]
 struct Season {
     season: i32,
     episodes: Vec<Episode>,
 }
 
+#[derive(Debug)]
 #[derive(Serialize)]
 struct Episode {
     id: String,
@@ -37,9 +41,8 @@ struct Episode {
     episode: i32,
 }
 
-#[get("/movies")]
-fn get_movies() -> Json<Vec<Movie>> {
-    let conn = Connection::open(DATABASE_PATH).unwrap();
+fn get_movies_from_db(database_path: &str) -> Vec<Movie> {
+    let conn = Connection::open(database_path).unwrap();
     let mut stmt = conn.prepare("SELECT id, title, year FROM movies").unwrap();
     let movies_iter = stmt
         .query_map(params![], |row| {
@@ -56,12 +59,11 @@ fn get_movies() -> Json<Vec<Movie>> {
         movies.push(movie.unwrap());
     }
 
-    Json(movies)
+    movies
 }
 
-#[get("/series")]
-fn get_series() -> Json<Vec<Series>> {
-    let conn = Connection::open(DATABASE_PATH).unwrap();
+fn get_series_from_db(database_path: &str) -> Vec<Series> {
+    let conn = Connection::open(database_path).unwrap();
     let mut stmt = conn
         .prepare("SELECT id, title, start_year, end_year FROM series")
         .unwrap();
@@ -82,20 +84,17 @@ fn get_series() -> Json<Vec<Series>> {
         series.push(serie.unwrap());
     }
 
-    Json(series)
+    series
 }
 
-#[get("/series/<id>")]
-fn get_series_by_id(id: String) -> Json<Series> {
-    let conn = Connection::open(DATABASE_PATH).unwrap();
-    print!("Fetching series with id: {}\n", id);
+fn get_series_seasons_from_db(database_path: &str, series_id: &str) -> Series {
+    let conn = Connection::open(database_path).unwrap();
     // Fetch episodes
     let mut stmt = conn
         .prepare("SELECT id, season, episode FROM episodes WHERE series_id = ?1")
         .unwrap();
-    print!("Fetching episodes\n");
     let episodes_iter = stmt
-        .query_map(params![id], |row| {
+        .query_map(params![series_id], |row| {
             Ok(Episode {
                 id: row.get(0)?,
                 season: row.get(1)?,
@@ -103,16 +102,12 @@ fn get_series_by_id(id: String) -> Json<Series> {
             })
         })
         .unwrap();
-    print!("Episodes fetched\n");
 
     // Create seasons and attach episodes
     let mut seasons: Vec<Season> = Vec::new();
     for episode in episodes_iter {
         let episode = episode.unwrap();
-        print!(
-            "season id {:?} episode number {:?} id {:?}\n",
-            episode.season, episode.episode, episode.id
-        );
+
         let season = seasons.iter_mut().find(|s| s.season == episode.season);
         match season {
             Some(season) => season.episodes.push(episode),
@@ -124,13 +119,12 @@ fn get_series_by_id(id: String) -> Json<Series> {
             }
         }
     }
-    print!("number of seasons {:?}\n", seasons.len());
 
     let mut stmt = conn
         .prepare("SELECT id, title, start_year, end_year FROM series WHERE id = ?1")
         .unwrap();
     let series = stmt
-        .query_row(params![id], |row| {
+        .query_row(params![series_id], |row| {
             Ok(Series {
                 id: row.get(0)?,
                 title: row.get(1)?,
@@ -140,11 +134,25 @@ fn get_series_by_id(id: String) -> Json<Series> {
             })
         })
         .unwrap();
-    print!(
-        "series id {:?} title {:?} start year {:?} end year {:?}\n",
-        series.id, series.title, series.start_year, series.end_year
-    );
 
+    series
+}
+
+#[get("/movies")]
+async fn get_movies() -> Json<Vec<Movie>> {
+    let movies = get_movies_from_db(DATABASE_PATH);
+    Json(movies)
+}
+
+#[get("/series")]
+async fn get_series() -> Json<Vec<Series>> {
+    let series = get_series_from_db(DATABASE_PATH);
+    Json(series)
+}
+
+#[get("/series/<id>")]
+async fn get_series_by_id(id: &str) -> Json<Series> {
+    let series = get_series_seasons_from_db(DATABASE_PATH, id);
     Json(series)
 }
 
