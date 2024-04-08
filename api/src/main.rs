77b30e9bc4b20f43e3d7efa5283bdc730137429a
Shahrad Laptop <shahrad.shj@gmail.com>
2024-04-08 12:23:15 -0700

    Refactor database_access module and main.rs to use database_access module functions for retrieving movies and series in the API endpoints

--- api/src/main.rs
+++ api/src/main.rs
@@ -5,25 +5,25 @@ extern crate rusqlite;
 mod database_access;
 
 use rocket::serde::json::Json;
-use crate::database_access::{Movie, Series, get_movies_from_db, get_series_from_db, get_series_seasons_from_db};
+use crate::database_access::{self as db_access, Movie, Series};
 
 const DATABASE_PATH: &str = "../movie-database/database.db";
 
 #[get("/movies")]
 async fn get_movies() -> Json<Vec<Movie>> {
-    let movies = get_movies_from_db(DATABASE_PATH);
+    let movies = db_access::get_movies_from_db(DATABASE_PATH);
     Json(movies)
 }
 
 #[get("/series")]
 async fn get_series() -> Json<Vec<Series>> {
-    let series = get_series_from_db(DATABASE_PATH);
+    let series = db_access::get_series_from_db(DATABASE_PATH);
     Json(series)
 }
 
 #[get("/series/<id>")]
 async fn get_series_by_id(id: &str) -> Json<Series> {
-    let series = get_series_seasons_from_db(DATABASE_PATH, id);
+    let series = db_access::get_series_seasons_from_db(DATABASE_PATH, id);
     Json(series)
 }
 
