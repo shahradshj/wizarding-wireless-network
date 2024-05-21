a84f17973edeb8ea26a7a867a9e44f0c425c5b34
Shahrad Windows-PC <shahrad.shj@gmail.com>
2024-05-21 15:05:30 -0700

    chore: Refactor get_suggestions function to handle movies and series together

--- movie-database/scanner/suggestions.py
+++ movie-database/scanner/suggestions.py
@@ -1,7 +1,5 @@
 import os
-import random
 import sys
-import asyncio
 from openai import AsyncOpenAI
 from dotenv import load_dotenv
 
@@ -17,59 +15,34 @@ MAX_FAVORITES = 10
 client = AsyncOpenAI(api_key=OPENAI_API_KEY)
 
 MODEL = "gpt-3.5-turbo"
-SYSTEM_MESSAGES_FOR_MOVIES = [
-    {"role": "system", "content": "You are a movies suggestion chatbot."},
-    {"role": "system", "content": "You are chatting with a user who is looking for a new movies to watch."},
-    {"role": "system", "content": "The user is asking for a suggestion, by giving you a list of movies they like."},
-    {"role": "system", "content": "Only responsd with a comma separated name of movies."},
-]
 
-SYSTEM_MESSAGES_FOR_SERIES = [
-    {"role": "system", "content": "You are a series suggestion chatbot."},
-    {"role": "system", "content": "You are chatting with a user who is looking for a new series to watch."},
-    {"role": "system", "content": "The user is asking for a suggestion, by giving you a list of series they like."},
-    {"role": "system", "content": "Only responsd with a comma separated name of series."},
-]
-
-async def get_movie_suggestions(movies: list[str]) -> list[str]:
-    if not movies or len(movies) == 0:
+async def get_suggestions(movies: list[str], series: list[str], favoriteMovies: list[str], favoriteSeries: list[str]) -> list[str]:
+    if (not favoriteMovies or len(favoriteMovies) == 0) and (not favoriteSeries or len(favoriteSeries) == 0):
         return []
-    if len(movies) > MAX_FAVORITES:
-        random_indices = random.sample(range(len(movies)), MAX_FAVORITES)
-        movies = [movies[i] for i in random_indices]
-    with_user_messages = SYSTEM_MESSAGES_FOR_MOVIES + [{"role": "user", "content": f"I like the movies: {', '.join(movies)}."}]
+    messages = [
+        {"role": "system", "content": "You are a movies and series suggestion system."},
+        {"role": "system", "content": f"Only these movies: {', '.join(movies)} and these series: {', '.join(series)} are available to suggest."},
+        {"role": "system", "content": "The user is asking for a suggestion, by giving you a list of movies and series they like."},
+        {"role": "system", "content": "Respond with a comma separated list of movies and series names. No other information is needed."},
+        {"role": "user", "content": f"I like these movies: {', '.join(favoriteMovies)}, and I like these series: {', '.join(favoriteSeries)}."},
+    ]
     try:
         suggestions = await client.chat.completions.create(
             model=MODEL,
-            messages=with_user_messages,
+            messages=messages,
         )
-        print("movie suggestions response", suggestions.choices[0].message.content)
+        print("suggestions response", suggestions.choices[0].message.content)
         return suggestions.choices[0].message.content.split(", ")
     except Exception as e:
+        print("Error getting suggestions", e)
         return []
-    
-async def get_series_suggestions(series: list[str]) -> list[str]:
-    if not series or len(series) == 0:
-        return []
-    if len(series) > MAX_FAVORITES:
-        random_indices = random.sample(range(len(series)), MAX_FAVORITES)
-        series = [series[i] for i in random_indices]
-    with_user_messages = SYSTEM_MESSAGES_FOR_SERIES + [{"role": "user", "content": f"I like the series: {', '.join(series)}."}]
-    try:
-        suggestions = await client.chat.completions.create(
-            model=MODEL,
-            messages=with_user_messages,
-        )
-        print("series suggestions response", suggestions.choices[0].message.content)
-        return suggestions.choices[0].message.content.split(", ")
-    except Exception as e:
-        print("Error getting series suggestions", e)
-        return []
-    
+
 async def find_suggestions_for_user(db_path: str, userId: str):
     suggested_movie_count = 0
     suggested_series_count = 0
     with DBAccess(db_path) as db:
+        allMovies = set([movie['name'] for movie in db.get_movies()])
+        allSeries = set([series['name'] for series in db.get_series()])
         favorite_movies = set([movie['name'] for movie in db.get_favorite_movies_by_user_id(userId)])
         favorite_series = set([series['name'] for series in db.get_favorite_series_by_user_id(userId)])
         print("userId", userId)
@@ -78,29 +51,25 @@ async def find_suggestions_for_user(db_path: str, userId: str):
 
         already_suggested = set(db.get_suggestions_by_user_id(userId))
 
-        movie_suggestions = await get_movie_suggestions(list(favorite_movies))
-        series_suggestions = await get_series_suggestions(list(favorite_series))
-        print(f"Movie suggestions: {movie_suggestions}")
-        print(f"Series suggestions: {series_suggestions}")
-
-
-        for suggested_movie in movie_suggestions:
-            found_movies = db.get_movies_by_name(suggested_movie)
-            for movie in found_movies:
-                if movie['id'] in already_suggested:
-                    continue
-                db.insert_suggestion(userId, movie['id'])
-                print(f"Inserted suggestion for movie {movie['name']}")
-                suggested_movie_count += 1
-
-        for suggested_series in series_suggestions:
-            found_seris = db.get_series_by_title(suggested_series)
-            for series in found_seris:
-                if series['id'] in already_suggested:
-                    continue
-                db.insert_suggestion(userId, series['id'])
-                print(f"Inserted suggestion for series {series['name']}")
-                suggested_series_count += 1
+        suggestions = await get_suggestions(list(allMovies), list(allSeries), list(favorite_movies), list(favorite_series))
+        print(f"Suggestions: {suggestions}")
+        for suggested in suggestions:
+            if suggested in allMovies:
+                found_movies = db.get_movies_by_name(suggested)
+                for movie in found_movies:
+                    if movie['id'] in already_suggested:
+                        continue
+                    db.insert_suggestion(userId, movie['id'])
+                    print(f"Inserted suggestion for movie {movie['name']}")
+                    suggested_movie_count += 1
+            elif suggested in allSeries:
+                found_series = db.get_series_by_title(suggested)
+                for series in found_series:
+                    if series['id'] in already_suggested:
+                        continue
+                    db.insert_suggestion(userId, series['id'])
+                    print(f"Inserted suggestion for series {series['name']}")
+                    suggested_series_count += 1
 
     return (suggested_movie_count, suggested_series_count)
 
